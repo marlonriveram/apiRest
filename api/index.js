e17df23950d1f90b782d43189ef0a23ea01bc2cb
marlon <marlon.mosquerar@gmail.com>
2024-03-12 16:16:42 -0500

    otra midificacion

--- api/index.js
+++ api/index.js
@@ -12,19 +12,22 @@ const port = process.env.PORT || 3000; // puerto por donde se realiza la peticio
 app.use(express.json());// nos permite recibir informacion enviado en el body con el metodo post
 
 
-const whitelist = ['http://localhost:5500']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(cors(corsOptions));
+// const whitelist = ['http://localhost:5500']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+app.use(cors());
 
 routerApi(app); // Funcion Manejadora de Rutas
+app.get('api/',(req,res)=>{
+  res.send('Hola mi api en express')
+})
 
 // middelwares
 app.use(logError);
